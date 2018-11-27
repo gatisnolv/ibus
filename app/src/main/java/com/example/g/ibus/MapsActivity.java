3f0a64d8352d449ad061125ced5a1eca5d7e8251
Dainis Bleidels <dainis.bleidels@gmail.com>
2018-11-27 12:10:55 +0200

    test commit, line 17

--- app/src/main/java/com/example/g/ibus/MapsActivity.java
+++ app/src/main/java/com/example/g/ibus/MapsActivity.java
@@ -14,6 +14,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    // test comment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
